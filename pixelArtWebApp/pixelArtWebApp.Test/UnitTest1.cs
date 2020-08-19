be83d78e2e45867e1a17928e351a963964238061
Michael Becker <michaelbecker@grupotechnos.com.br>
2020-08-19 18:52:22 -0300

    chore: add test project with docker

---
+++ pixelArtWebApp/pixelArtWebApp.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace pixelArtWebApp.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.False(false, "not false");
+        }
+    }
+}
