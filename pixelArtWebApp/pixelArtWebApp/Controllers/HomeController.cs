e45a0f2710483f2c9c399b1930db55f3b4e82f8c
Michael Becker <michaelbecker@grupotechnos.com.br>
2020-08-20 14:35:55 -0300

    feat: simple image upload

--- pixelArtWebApp/pixelArtWebApp/Controllers/HomeController.cs
+++ pixelArtWebApp/pixelArtWebApp/Controllers/HomeController.cs
@@ -1,20 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using pixelArtWebApp.Models;
+using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace pixelArtWebApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IHostEnvironment _env;
+        public IActionResult SingleFile(IFormFile file)
         {
+            var dir = _env.ContentRootPath;
+            using (var fileStream = new FileStream(Path.Combine(dir + "/wwwroot/uploads", string.Format(@"{0}.png", DateTime.Now.Ticks)), FileMode.Create, FileAccess.Write))
+            {
+                file.CopyTo(fileStream);
+            }
+            return RedirectToAction("Index");
+        }
+        public HomeController(IHostEnvironment env, ILogger<HomeController> logger)
+        {
+            _env = env;
             _logger = logger;
         }
 
